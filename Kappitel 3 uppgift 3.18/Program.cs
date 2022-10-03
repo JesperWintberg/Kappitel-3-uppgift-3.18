7f0ebaf83b9c6bbb37cf10709e16199c7825daf0
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-03 18:40:35 +0200

    Add project files.

---
+++ Kappitel 3 uppgift 3.18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt för och efternamn");
+            string mening = Console.ReadLine();
+
+            string[] ord = mening.Split(' ');
+
+            string[] names = { ord[0], ord[1] };
+            var sortedNames = names.OrderBy(n => n);
+            foreach (var name in sortedNames)
+            {
+                Console.WriteLine(name);    
+            } 
+        }
+    }
+}
+
